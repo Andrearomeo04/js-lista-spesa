395a556a3092de23474f82514e7314211edb0466
andrearomeo04 <pisco004@gmail.com>
2024-07-02 22:02:09 +0200

    [UPD] cicled the array with while

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const ingredients = ['uova', 'pecorino', 'guanciale', 'pepe nero', 'pasta']
+const ingredients = ['uova', 'pecorino', 'guanciale', 'pepe nero', 'pasta'];
 
-const grocery_list = document.getElementById('grocery_list')
+const grocery_list = document.getElementById('grocery_list');
+
+let i = 0;
+
+while (i < ingredients.length) {
+    const li = document.createElement('li');
+    li.innerText = ingredients[i];
+    grocery_list.append(li);
+    i++
+}
